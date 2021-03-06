dcaaa64e672fb3a9ba0f6ffb2d1fba3c03bec2bd
DESKTOP-2289HQC\DELL <quangklynk@gmail.com>
2021-03-07 00:04:52 +0700

    hoàn thành

--- js/manage.js
+++ js/manage.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
-    var active_star;
-    var inside_star;
-    let flat = true;
-    $(".show-popup").click(function () {
-      if (flat === true) {
-        active_star = $(this);
-        inside_star = active_star.closest(".customer").find(".container-customer");
-        $(inside_star).fadeIn();
-        flat = false;
-      }
-    });
-    $(".close-popup").click(function () {
-      if (flat === false) {
-        $(inside_star).fadeOut();
-        flat = true;
-      }
-    });
+  let curren;
+  let inside;
+  let flat = true;
+  $(".show-popup").click(function () {
+    if (flat === true) {
+      curren = $(this);
+      inside = curren
+        .closest(".customer")
+        .find(".container-customer");
+      $(inside).fadeIn();
+      flat = false;
+    }
   });
+  $(".close-popup").click(function () {
+    if (flat === false) {
+      $(inside).fadeOut();
+      flat = true;
+    }
+  });
+});
